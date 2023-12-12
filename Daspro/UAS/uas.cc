fddc961632f5cdb6968d8354d72095f4292131a6
Pfftz <Kalixpfft+github@protonmail.com>
2023-12-13 00:28:37 +0700

    Add rock-paper-scissors game logic

--- Daspro/UAS/uas.cc
+++ Daspro/UAS/uas.cc
@@ -1,8 +1,39 @@
 #include <iostream>
-
-using namespace std;
+#include <ctime>
+#include <cstdlib>
 
 int main()
 {
+    srand(time(0)); // Initialize random number generator.
+
+    std::string moves[3] = {"Rock", "Paper", "Scissors"};
+
+    while (true)
+    {
+        std::cout << "Enter your move (Rock, Paper, Scissors): ";
+        std::string user_move;
+        std::cin >> user_move;
+
+        int random_num = rand() % 3; // Generate a random number between 0 and 2.
+        std::string computer_move = moves[random_num];
+
+        std::cout << "Computer chose: " << computer_move << std::endl;
+
+        if (user_move == computer_move)
+        {
+            std::cout << "It's a tie!" << std::endl;
+        }
+        else if ((user_move == "Rock" && computer_move == "Scissors") ||
+                 (user_move == "Scissors" && computer_move == "Paper") ||
+                 (user_move == "Paper" && computer_move == "Rock"))
+        {
+            std::cout << "You win!" << std::endl;
+        }
+        else
+        {
+            std::cout << "You lose!" << std::endl;
+        }
+    }
+
     return 0;
 }
