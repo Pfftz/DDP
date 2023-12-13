9bba3a44386e294c0eb2140620570cd5578f93b7
Pfftz <Kalixpfft+github@protonmail.com>
2023-12-13 09:12:52 +0700

    Refactor code to check for palindrome

--- Daspro/UAS/uas.cc
+++ Daspro/UAS/uas.cc
@@ -1,39 +1,42 @@
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
+#include <string>
+#include <stack>
+using namespace std;
+
+string kalimat;
+stack<char> kata;
+bool palindrom = true;
 
 int main()
 {
-    srand(time(0)); // Initialize random number generator.
+    cout << " Masukkan Kalimat: ";
+    getline(cin, kalimat);
+    cout << "\n Jumlah kata dari kalimat yang dimasukkan: " << kalimat.length() << endl;
 
-    std::string moves[3] = {"Rock", "Paper", "Scissors"};
-
-    while (true)
+    // Memasukkan setiap karakter ke dalam stack
+    for (int i = 0; i < kalimat.length(); i++)
     {
-        std::cout << "Enter your move (Rock, Paper, Scissors): ";
-        std::string user_move;
-        std::cin >> user_move;
-
-        int random_num = rand() % 3; // Generate a random number between 0 and 2.
-        std::string computer_move = moves[random_num];
-
-        std::cout << "Computer chose: " << computer_move << std::endl;
-
-        if (user_move == computer_move)
-        {
-            std::cout << "It's a tie!" << std::endl;
-        }
-        else if ((user_move == "Rock" && computer_move == "Scissors") ||
-                 (user_move == "Scissors" && computer_move == "Paper") ||
-                 (user_move == "Paper" && computer_move == "Rock"))
-        {
-            std::cout << "You win!" << std::endl;
-        }
-        else
-        {
-            std::cout << "You lose!" << std::endl;
-        }
+        kata.push(kalimat[i]);
     }
 
+    // Membandingkan elemen pertama dan terakhir pada stack
+    for (int i = 0; i < kalimat.length() / 2; i++)
+    {
+        if (kata.top() != kalimat[i])
+        {
+            palindrom = false;
+            break;
+        }
+        kata.pop();
+    }
+
+    if (palindrom)
+    {
+        cout << "\n Kalimat tersebut palindrom " << endl;
+    }
+    else
+    {
+        cout << "\n Kata tersebut bukan palindrom" << endl;
+    }
     return 0;
 }
