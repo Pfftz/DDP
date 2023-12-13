642aca86e1cd21831a0e1881472b034191739903
Pfftz <Kalixpfft+github@protonmail.com>
2023-12-13 20:47:20 +0700

    Update welcome message and fix loop index type

--- Daspro/UAS/uas.cc
+++ Daspro/UAS/uas.cc
@@ -16,25 +16,36 @@ void welcome()
     untuk menyertakan karakter khusus dalam teks tanpa harus meng-escape-nya (menambahkan karakter khusus seperti backslash '\').
     */
     string opening = R"(
-                                                                                                                
-                                                                        ,,                                      
-        `7MMF'  `7MMF'                           `7MM"""YMM             db                                      
-          MM      MM                               MM    `7                                                     
-          MM      MM  .gP"Ya `7M'   `MF' ,6"Yb.    MM   d   `7MMpdMAo.`7MM  ,p6"bo `7MM  `7MM  `7Mb,od8 .gP"Ya  
-          MMmmmmmmMM ,M'   Yb  `VA ,V'  8)   MM    MMmmMM     MM   `Wb  MM 6M'  OO   MM    MM    MM' "',M'   Yb 
-          MM      MM 8M""""""    XMX     ,pm9MM    MM   Y  ,  MM    M8  MM 8M        MM    MM    MM    8M"""""" 
-          MM      MM YM.    ,  ,V' VA.  8M   MM    MM     ,M  MM   ,AP  MM YM.    ,  MM    MM    MM    YM.    , 
-        .JMML.  .JMML.`Mbmmd'.AM.   .MA.`Moo9^Yo..JMMmmmmMMM  MMbmmd' .JMML.YMbmd'   `Mbod"YML..JMML.   `Mbmmd' 
-                                                              MM                                                
-                                                            .JMML.                                              
+                                                                                                           
+        ##### ##           ###                          ##                                                 
+     ######  /###           ###    #                     ##                                                
+    /#   /  /  ###           ##   ###                    ##                                                
+   /    /  /    ###          ##    #                     ##                                                
+       /  /      ##          ##                          ##                                                
+      ## ##      ##  /###    ##  ###   ###  /###     ### ##  ###  /###     /###   ### /### /###     /##    
+      ## ##      ## / ###  / ##   ###   ###/ #### / ######### ###/ #### / / ###  / ##/ ###/ /##  / / ###   
+    /### ##      / /   ###/  ##    ##    ##   ###/ ##   ####   ##   ###/ /   ###/   ##  ###/ ###/ /   ###  
+   / ### ##     / ##    ##   ##    ##    ##    ##  ##    ##    ##       ##    ##    ##   ##   ## ##    ### 
+      ## ######/  ##    ##   ##    ##    ##    ##  ##    ##    ##       ##    ##    ##   ##   ## ########  
+      ## ######   ##    ##   ##    ##    ##    ##  ##    ##    ##       ##    ##    ##   ##   ## #######   
+      ## ##       ##    ##   ##    ##    ##    ##  ##    ##    ##       ##    ##    ##   ##   ## ##        
+      ## ##       ##    /#   ##    ##    ##    ##  ##    /#    ##       ##    ##    ##   ##   ## ####    / 
+      ## ##        ####/ ##  ### / ### / ###   ###  ####/      ###       ######     ###  ###  ### ######/  
+ ##   ## ##         ###   ##  ##/   ##/   ###   ###  ###        ###       ####       ###  ###  ### #####   
+###   #  /                                                                                                 
+ ###    /                                                                                                  
+  #####/                                                                                                   
+    ###                                                                                                    
 )";
     // fontnya NV Script
     cout << endl
          << opening << "\n"
          << endl;
-    cout << "                    Welcome to HexaEpicure! Experience culinary magic with the essence of six. \n\n\n";
-    cout << "  \"Embrace the harmony of HexaEpicure, where the essence of six transforms every bite into a symphony of perfection.\"\n\n\n"
-         << endl;
+    cout << "====================================" << endl;
+    cout << " Nama: Abdulhadi Muntashir" << endl;
+    cout << " NIM: 3337230041" << endl;
+    cout << "====================================" << endl;
+    cout << endl;
     system("pause"); // command line for stopping the program
 }
 
@@ -48,13 +59,13 @@ int main()
     cout << "\n Jumlah kata dari kalimat yang dimasukkan: " << kalimat.length() << endl;
 
     // Memasukkan setiap karakter ke dalam stack
-    for (int i = 0; i < kalimat.length(); i++)
+    for (std::string::size_type i = 0; i < kalimat.length(); i++)
     {
         kata.push(kalimat[i]);
     }
 
     // Membandingkan elemen pertama dan terakhir pada stack
-    for (int i = 0; i < kalimat.length() / 2; i++)
+    for (std::string::size_type i = 0; i < kalimat.length() / 2; i++)
     {
         if (kata.top() != kalimat[i])
         {
